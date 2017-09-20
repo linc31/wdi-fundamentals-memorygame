66e72e5e34d4de135a1d8f2e05ea59d10a7f34fa
Linc <christinalin31@gmail.com>
2017-09-19 19:21:37 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-var cardOne = [0];
-cardsInPlay.push('cardOne');
-console.log("User flipped Queen");
 
-var cardTwo = [2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped King");
-
+//check for matching card
+var checkForMatch = function (){
 if (cardsInPlay.length === 2){
 	if (cardsInPlay[0] === cardsInPlay[1]){
 	alert("You found a match!");
@@ -16,12 +11,21 @@ if (cardsInPlay.length === 2){
 	alert ("Sorry, try again!");
 	}
 }
+}
 
+// flips a card
+var flipCard = function (){
+	var cardId = [];
+	cardsInPlay.push(cards[cardId]);
+ checkForMatch();
+ console.log("User flipped " + cards[cardId]);
+}
+flipCard(0);
 
+var cardOne = [0];
+cardsInPlay.push('cardOne');
+console.log("User flipped Queen");
 
-
-
-
-
-
-
+var cardTwo = [2];
+cardsInPlay.push('cardTwo');
+console.log("User flipped King");
